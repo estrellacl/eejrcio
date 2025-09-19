6aab5ce7975029b288bfebfb419dffbff7d072f6
estrellacl <N00477869@upn.pe>
2025-09-19 10:26:43 -0500

    Add files via upload

---
+++ EJER1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA4_C_
+{
+    internal class EJER1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INGRESE SU EDAD :");
+            int edad= int.Parse(Console.ReadLine());
+
+            if(edad < 18)
+            {
+                Console.WriteLine("MENOR DE EDAD");
+
+            }
+            else
+            {
+                if (edad >= 18 && edad <= 64) 
+                {
+                    Console.WriteLine("ADULTO");
+                }
+                else
+                {
+                    Console.WriteLine("ADULTO MAYOR");
+
+                }
+
+                Console.ReadKey();
+            }
+
+
+            
+        }
+    }
+}
