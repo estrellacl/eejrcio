590a9b9accada6d9fb86b4ceed5d51a227fb13d6
estrellacl <N00477869@upn.pe>
2025-09-19 10:34:07 -0500

    Add files via upload

---
+++ EJER_1.py
@@ -0,0 +1,19 @@
+edad= int(input("ingrese la edad:"))
+
+print()
+
+if edad <18:
+    print("No puede votar")
+    print("no puede ser candidato")
+elif edad < 25 :
+    print("Puede votar")
+    print ("No puede ser candidato")
+else:
+    print("puede votar")
+    print ("puede ser candidato")
+
+
+
+
+
+
